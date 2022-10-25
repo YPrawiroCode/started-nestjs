4b1e9d9b5dde09e8d6cdac52a526d6f09c06c7ea
Prawiro <yusufprawiro14@gmail.com>
2022-10-25 18:05:30 +0700

    update

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,20 +1,40 @@
 /* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import {
   ExtractJwt,
   Strategy,
 } from 'passport-jwt';
-import { config } from 'process';
+import { PrismaService } from 'src/prisma/prisma.service';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(
   Strategy,
+  'jwt',
 ) {
-  constructor() {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
     super({
       jwtFromRequest:
         ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      // ignoreExpiration: false,
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+  async validate(payload: {
+    sub: number;
+    email: string;
+  }) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          id: payload.sub,
+        },
+      });
+    delete user.hash;
+    return user;
   }
 }
