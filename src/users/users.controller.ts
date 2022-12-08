dea521179702d2c4a45399979a1629849d30c755
YPrawiroCode <yusufprawiro14@gmail.com>
2022-12-08 16:51:49 +0700

    update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     console.log(
-      '🚀 ~ file: users.controller.ts:8 ~ UsersController ~ createUser ~ body',
+      '🚀 ~ file: users.controller.ts:11 ~ UsersController ~ createUser ~ body',
       body,
     );
+    this.usersService.create(body.email, body.password);
   }
 }
